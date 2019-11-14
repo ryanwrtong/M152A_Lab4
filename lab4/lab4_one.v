33d9ffb1bbde72b90698096ed4d26bd274678eff
nathanachen <nathanachen99@gmail.com>
2019-11-14 15:08:57 -0800

    initial

---
+++ lab4/lab4_one.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:40:50 11/14/2019 
+// Design Name: 
+// Module Name:    lab4_one 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module lab4_one(
+    );
+
+
+endmodule
